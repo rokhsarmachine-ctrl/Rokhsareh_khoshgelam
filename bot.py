637b958fab90837acfd4041c06659e68d6550c78
rokhsarmachine-ctrl <rokhsar.machine@gmail.com>
2026-09-25 23:02:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ ADMIN_ID = 8070693669
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    welcome_text = "به ربات خدمات مزووایت رخساره خانوم خوشگل 🥰 خوش اومدی"
+    welcome_text = "به ربات خدمات مزووایت رخساره خانوم خوشگل🥰 خوش اومدی"
 
     try:
         photos = bot.get_user_profile_photos(bot.get_me().id)
